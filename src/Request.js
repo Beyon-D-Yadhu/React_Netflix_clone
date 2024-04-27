8cd399e64bd99f463e4db42457e9804dd1f5f50c
Yadhu-krishnan <yadhuy28@gmail.com>
2024-04-27 10:59:04 +0530

    Again changes

--- src/Request.js
+++ src/Request.js
@@ -1,5 +1,7 @@
 // require('')
+
 export const key = '383da997f2eb32cd621df7683b25340b'
+
 //https://api.themoviedb.org/3/discover/movie?api_key=383da997f2eb32cd621df7683b25340b&language=pt-BR&page=1&with_genres=28
 const requests = {
     Action:`https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=US-BR&page=1&with_genres=28`,
